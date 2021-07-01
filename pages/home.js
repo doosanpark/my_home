cfedc5904752f34c91927d76769a9af5667e2cd8
doosanpark <erid3232@naver.com>
2021-07-02 00:27:06 +0900

    home에 className  일부 적용

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,12 @@
 import React from  'react';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
-
+import styles from '/styles/home.module.css';
+import classNames from "classnames";
 function Home(){
     const router = useRouter();
     return (
-        <div className={"formLayout"} style={{ alignItems: "center"}}>
+        <div style={{ alignItems: "center"}}>
             <h4>우리집 가격이 궁금할 때는?</h4>
             <h1>우리집</h1>
 
@@ -16,7 +17,7 @@ function Home(){
                 <a>이미 계정이 있으신가요?</a>
                 </Link>
             </div>
-            <button>가입하기</button>
+            <button className={classNames({[styles.btn]: true}, {[styles.hi]: true})}>가입하기</button>
 
         </div>
         
