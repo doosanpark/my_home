74b9829c5b89722c349d1fa9e98edce784f0ce48
SYKim <syk0707@naver.com>
2021-07-01 14:53:55 +0900

    [search] 개별 item View 추가중

---
+++ pages/search/homeItem.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from "react";
+import Link from "next/link";
+import { useRouter } from 'next/router'
+import searchStyles from "/styles/search.module.css";
+import axios from 'axios';
+
+function HomeItem() {
+    return <></>
+}
+
+export default HomeItem;
