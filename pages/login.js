65c60a7743c7fd395cd44991969927e5c4da1824
doosanpark <erid3232@naver.com>
2021-07-10 04:31:53 +0900

    로그인 하던 도중 연동 실패

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 function Login(){
 
+
+    const [telInfo, setTelInfo] = useState();
+    const [passInfo, setPassInfo] = useState();
+
+    const fnLogin = () => {
+        if(telInfo === "" || passInfo === ""){
+            alert("항목을 모두 입력하세요");
+            return;
+        }
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/account/listmap',
+            data: {
+                telInfo: telInfo
+                , passInfo: passInfo
+            }
+          }).then((res) => {
+            console.log("res", res);
+          }).catch(function(err) {
+            console.log("err", err);
+        });  
+
+    }
+
     return (
-        <div className="container">
-            <div className="baseFormat">
-                <div>
-                    <div>핸드폰 번호</div>
-                    <input placeholder={"핸드폰 번호를 입력해주세요"}></input>
-                </div>
-                <div>
-                    <div>비밀번호</div>
-                    <input placeholder={"비밀번호를 입력해주세요"}></input>
-                    <div><a>비밀번호 찾기</a></div>
-                </div>
+        <div className="container baseFormat">
+            <section className={"top-sector"}>
+                <div className={"input-group"}><span className="fs-5">핸드폰 번호</span></div>
+                <input className={"form-control"} placeholder={"핸드폰 번호를 입력해주세요"}
+                onChange={async (e) => {
+                    const value = e.currentTarget
+                    setTelInfo(value);
+                }}/>
+            </section>
+            <section className={"top-sector"}>
+                <div><span className={["fs-5"]}>비밀번호</span></div>
+                <input className={"form-control"} placeholder={"비밀번호를 입력해주세요"}
+                onChange={async (e) => {
+                    const value = e.currentTarget
+                    setPassInfo(value);
+                }}/>
+                <div className={"text-right", "width-full"}><a>비밀번호 찾기</a></div>
+            </section>
+            
+            <section className={"top-sector"}>
                 
-                <div>
-                    <button className="btnDefault">로그인</button>
-                    <button className="btnActive">비밀번호</button>
-                </div>
-            </div>
+                <button className="btnDefault" onClick={fnLogin}>로그인</button>
+                <button className="btnActive">비밀번호</button>
+            </section>
         </div>
     );
 }
