c1a3a91935d937682c6f559cc86a440f74ee3aac
doosanpark <erid3232@naver.com>
2021-10-06 23:43:56 +0900

    cors 문제 해결

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://localhost:3001/:path*',
+      },
+    ]
+  },
 }
--- pages/login.js
+++ pages/login.js
@@ -1,27 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
-function Login(){
+function Login() {
 
     const [telInfo, setTelInfo] = useState();
     const [passInfo, setPassInfo] = useState();
     const router = useRouter();
     const fnLogin = () => {
-        if(telInfo === "" || passInfo === ""){
+        if (telInfo === "" || passInfo === "") {
             alert("항목을 모두 입력하세요");
             return;
         }
-
+        console.log("telInfo", telInfo);
+        console.log("passInfo", passInfo);
         axios({
             method: 'post',
-            url: 'http://localhost:8080/account/selAccntExist',
+            url: 'http://localhost:3001/api/account/login',
             data: {
                 telInfo: telInfo
                 , passInfo: passInfo
-            } 
+            },
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+            }
+
         }).then(res => {
-            if(res.data.result > 0){
+            if (res.data.result > 0) {
                 alert("로그인되었습니다.");
                 var url = "/search/searchMain"
                 router.push(url);
@@ -34,24 +40,24 @@ function Login(){
             <section className={"top-sector"}>
                 <div className={"input-group"}><span className="fs-5">핸드폰 번호</span></div>
                 <input className={"form-control"} placeholder={"핸드폰 번호를 입력해주세요"}
-                onChange={async (e) => {
-                    const {value} = e.currentTarget
-                    setTelInfo(value);
-                }}/>
+                    onChange={async (e) => {
+                        const { value } = e.currentTarget
+                        setTelInfo(value);
+                    }} />
             </section>
             <section className={"top-sector"}>
                 <div><span className={["fs-5"]}>비밀번호</span></div>
                 <input type="password" className={"form-control"} placeholder={"비밀번호를 입력해주세요"}
-                onChange={async (e) => {
-                    const {value} = e.currentTarget
-                    setPassInfo(value);
-                }}
+                    onChange={async (e) => {
+                        const { value } = e.currentTarget
+                        setPassInfo(value);
+                    }}
                 />
                 <div className={"text-right", "width-full"}><a>비밀번호 찾기</a></div>
             </section>
-            
+
             <section className={"top-sector"}>
-                
+
                 <button className="btnDefault" onClick={fnLogin}>로그인</button>
                 <button className="btnActive">비밀번호 찾기</button>
             </section>
@@ -61,9 +67,9 @@ function Login(){
 
 export default Login;
 
-export async function getServerSideProps(context){
- 
-    return{
+export async function getServerSideProps(context) {
+
+    return {
         props: {}
     }
 
